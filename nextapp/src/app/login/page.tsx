ae2da9019a0510d467f20e36d3090ff1bfb7ea93
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-05-05 17:55:40 +0530

    Database connected successfully

---
+++ nextapp/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function LoginPage(){
+    return (
+        <div className="flex">
+            <h1 className="text-center text-white text-2xl">Login</h1>
+        </div>
+    );
+}
+
+// create a folder for every single route
+// meaning: if u want /login route create a folder in tht name and it shd always be in page.tsx(compulsorily)
