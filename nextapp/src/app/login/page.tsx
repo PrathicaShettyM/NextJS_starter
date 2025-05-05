2dd5e0f1f739cfe2d3da11a2d6c61beda0ea1070
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2025-05-05 20:47:43 +0530

    Some frontend part of nextjs covered

--- nextapp/src/app/login/page.tsx
+++ nextapp/src/app/login/page.tsx
@@ -1,10 +1,59 @@
+"use client"; // decorator
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function LoginPage(){
+    // collect these data from the user
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+    });
+
+    // handles the signup login(also communicates with the backend for the same)
+    const onLogin = async () => {
+
+    }
+
+
+
     return (
-        <div className="flex">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-center text-white text-2xl">Login</h1>
+            <hr/>
+
+            <label htmlFor="email">Email</label>
+            <input
+            className="p-1 border border-gray-300 rounded-lg mb-2"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+                placeholder="email"
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+            className="p-1 border border-gray-300 rounded-lg mb-2"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
+                placeholder="password"
+            />
+
+            <button
+            onClick={onLogin}
+            className="p-1 border border-green-400 rounded-lg mb-4 hover:bg-green-400 hover:text-black cursor-pointer"
+            >Login</button>
+            
+            <Link href="/signup">Visit SignUp Page</Link>
         </div>
     );
 }
+
 
 // create a folder for every single route
 // meaning: if u want /login route create a folder in tht name and it shd always be in page.tsx(compulsorily)
--- nextapp/src/app/signup/page.tsx
+++ nextapp/src/app/signup/page.tsx
@@ -1,7 +1,66 @@
+"use client"; // decorator
+
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignUpPage(){
+    // collect these data from the user
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: ""
+    });
+
+    // handles the signup login(also communicates with the backend for the same)
+    const onSignUp = async () => {
+
+    }
+
+
+
     return (
-        <div className="flex">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1 className="text-center text-white text-2xl">SignUp</h1>
+            <hr/>
+
+            <label htmlFor="username">Username</label>
+            <input
+            className="p-1 border border-gray-300 rounded-lg mb-2"
+                id="username"
+                type="text"
+                value={user.username}
+                onChange={(e) => setUser({...user, username: e.target.value})}
+                placeholder="username"
+            />
+
+            <label htmlFor="email">Email</label>
+            <input
+            className="p-1 border border-gray-300 rounded-lg mb-2"
+                id="email"
+                type="text"
+                value={user.email}
+                onChange={(e) => setUser({...user, email: e.target.value})}
+                placeholder="email"
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+            className="p-1 border border-gray-300 rounded-lg mb-2"
+                id="password"
+                type="password"
+                value={user.password}
+                onChange={(e) => setUser({...user, password: e.target.value})}
+                placeholder="password"
+            />
+
+            <button
+            onClick={onSignUp}
+            className="p-1 border border-green-400 rounded-lg mb-4 hover:bg-green-400 hover:text-black cursor-pointer"
+            >SignUp</button>
+            
+            <Link href="/login">Visit Login Page</Link>
         </div>
     );
 }
